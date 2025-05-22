6a7e4bb9aa8a963fb67c6a2a05be3514f5eb13b9
Agorbanoff <alexandargorbanov@gmail.com>
2025-05-22 21:09:57 +0300

    some code

--- test.py
+++ test.py
@@ -1,14 +1,1 @@
-from flask import Flask, redirect, url_for
-
-app = Flask(__name__)
-
-@app.route('/')
-def root():
-    return redirect(url_for('home'))
-
-@app.route('/home')
-def home():
-    return "Welcome to the Home Page!"
-
-if __name__ == "__main__":
-    app.run(debug=True)
+=
